16b6d4183369cefbe943d97b023ed1b3acc40525
blebeuf <bryanlebeuf@gmail.com>
2024-03-29 21:23:24 -0500

    added a delete buttion function

--- Develop/routes/note.js
+++ Develop/routes/note.js
@@ -1,8 +1,8 @@
 const notesRouter = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile, writeFromFile } = require('../helper/fsUtil');
+const { readAndAppend, readFromFile, writeToFile } = require('../helper/fsUtil');
 
-// GET Route for retrieving all the feedback
+// GET Route for retrieving all the notes
 notesRouter.get('/', (req, res) =>
   readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)))
 );
@@ -35,21 +35,21 @@ notesRouter.post('/', (req, res) => {
 });
 
 // notes on this piece https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-notesRouter.delete('/:id', (req,res) => {
-    const noteId = req.params.ids;
-
+notesRouter.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+  
     readFromFile('./db/notes.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-    // Filter out the note with the id to delete
-    const filteredNotes = json.filter((note) => note.id !== noteId);
-
-    writeFromFile('./db/notes.json', filteredNotes);
-
-    res.json(`Note with an id %{noteId} has been deleted.`);
-    
-    })
-    .catch((error) => res.status(500).json('Error in deleting note: ' + error));
-});
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Filter out the note with the id to delete
+        const filteredNotes = json.filter((note) => note.id !== noteId);
+  
+        // Write the filtered notes back to the file
+        writeToFile('./db/notes.json', filteredNotes);
+  
+        res.json(`Note with id ${noteId} has been deleted.`);
+      })
+      .catch((err) => res.status(500).json('Error in deleting note: ' + err));
+  });
 
 module.exports = notesRouter;
